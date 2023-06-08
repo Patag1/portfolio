04a70e84ea6e98b194ee24d209ad28020171f367
Augusto <augustosasso97@gmail.com>
2023-06-08 02:54:47 -0300

    special chars

--- src/app/hire/page.tsx
+++ src/app/hire/page.tsx
@@ -8,9 +8,9 @@ interface pageProps {}
 const page: FC<pageProps> = ({}) => {
   return <div className='bg-neutral-900 w-full h-full'>
     <Container width='w-full' className='relative animate-fade-up animate-once animate-duration-1000 animate-delay-500'>
-      <Header size={'xl'} className='absolute -top-20 left-8 text-neutral-400'>LET'S TALK, LET'S DO</Header>
+      <Header size={'xl'} className='absolute -top-20 left-8 text-neutral-400'>LET&apos;S TALK, LET&apos;S DO</Header>
       <div className='absolute top-56 left-9 right-9'>
-        <p className='mb-6 text-sm font-[Gambetta] text-neutral-400'>Let's make your brand <span className='font-extrabold'>glow</span></p>
+        <p className='mb-6 text-sm font-[Gambetta] text-neutral-400'>Let&apos;s make your brand <span className='font-extrabold'>glow</span></p>
         <ContactForm className='flex justify-center items-start flex-col' />
       </div>
     </Container>
